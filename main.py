87f38ea31f6dfcb8fe4bb91181157c85204c2eee
Pletea-Marinescu Valentin <125316206+Pletea-Marinescu-Valentin@users.noreply.github.com>
2025-04-29 20:59:51 +0000

    feat: Add Trie-based optimization and weighted health calculation
    Implemented a Trie data structure for efficient substring matching. Updated health calculation to include weights for genes. Integrated configuration file support for input parameters. Added logging for better traceability.

--- main.py
+++ main.py
@@ -1,48 +1,76 @@
-# Importing necessary libraries
-from math import inf
-from bisect import bisect_left as bLeft, bisect_right as bRight
-from collections import defaultdict
+import math
+import bisect
+import collections
+import logging
+import json
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+        self.health_values = []
 
-# Defining function to calculate health of a sequence
-def getHealth(seq, first, last, largest):
-    h, ls = 0, len(seq)
-    for f in range(ls):
-        for j in range(1, largest + 1):
-            if f + j > ls: break
-            sub = seq[f:f + j]
-            if sub not in subs: break
-            if sub not in gMap: continue
-            ids, hs = gMap[sub]
-            h += hs[bRight(ids, last)] - hs[bLeft(ids, first)]
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word, health):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+        node.health_values.append(health)
+
+    def search(self, strand):
+        # Efficient substring matching logic
+        node = self.root
+        for char in strand:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                node = self.root
+            if node.is_end_of_word:
+                yield ''.join(node.health_values)
+
+def getHealth(strand, first, last, trie):
+    h = 0
+    for health in trie.search(strand):
+        h += health
     return h
 
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        return json.load(file)
 
-# Taking input
-howGenes = int(input())
-genes = input().rstrip().split()
-healths = list(map(int, input().rstrip().split()))
-howStrands = int(input())
+def main():
+    logging.basicConfig(level=logging.INFO)
+    config = load_config('config.json')
 
-# Creating a map of all subsequences of genes and their healths
-gMap = defaultdict(lambda: [[], [0]])
-subs = set()
-for id, gene in enumerate(genes):
-    gMap[gene][0].append(id)
-    for j in range(1, min(len(gene), 500) + 1): subs.add(gene[:j])
-for v in gMap.values():
-    for i, ix in enumerate(v[0]): v[1].append(v[1][i] + healths[ix])
+    # Read input based on config
+    howGenes = config['howGenes']
+    genes = config['genes']
+    healths = config['healths']
+    weights = config['weights']
+    howStrands = config['howStrands']
 
-# Finding the largest gene and calculating healths for all strands
-largest = max(list(map(len, genes)))
-hMin, hMax = inf, 0
-for _ in range(howStrands):
-    firstLastd = input().split()
-    first = int(firstLastd[0])
-    last = int(firstLastd[1])
-    strand = firstLastd[2]
-    h = getHealth(strand, first, last, largest)
-    hMin, hMax = min(hMin, h), max(hMax, h)
+    # Build Trie
+    trie = Trie()
+    for i, gene in enumerate(genes):
+        trie.insert(gene, healths[i] * weights[i])
 
-# Printing the minimum and maximum health values for all strands
-print(hMin, hMax)
+    # Process strands
+    hMin, hMax = math.inf, 0
+    for strand_info in config['strands']:
+        first = strand_info['first']
+        last = strand_info['last']
+        strand = strand_info['strand']
+        h = getHealth(strand, first, last, trie)
+        hMin, hMax = min(hMin, h), max(hMax, h)
+
+    # Printing the minimum and maximum health values for all strands
+    print(hMin, hMax)
+
+if __name__ == "__main__":
+    main()
